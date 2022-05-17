3202b7f4915f9399cdfd75db8fef51522816dfde
gjTang <yanghs1109@gmail.com>
2022-05-17 11:05:46 +0900

    code update

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# yes24_aladin_crawler
-Yes24, 알라딘 최신 도서 crawling
+# Yes24,  Action with Python
+
+## Info
+- 변성윤님 코드 사용
+- 알라딘 최신 도서 수집 코드 추가
+
+## Description
+- (1) YES24/Aladin cron Github Action
+  - YES24 IT 신간 도서에 있는 TOP 40을 가져와서, 해당 Github Issue 업로드
+  - 알라딘 컴퓨터/모바일 섹션 (출간일 4주 이내, 50개 필터링)을 가져와서, 해당 Github Issue 업로드
+  - 매일 오전 9시에 업로드(한국 시간)
+  - Watch 클릭시 이메일로 알람받을 수 있음
+- (2) hello.py 실행하는 Github Action
+  - Master로 Push할 때 실행
+- (3) Job에서 생성한 파일을 공유해서 사용하는 Github Action
+  - [upload-artifact](https://github.com/actions/upload-artifact)와 [download-artifact](https://github.com/actions/download-artifact) 사용 예제
---
+++ main.py
@@ -0,0 +1,34 @@
+import os
+from datetime import datetime
+from pytz import timezone
+from crawling import extract_book_data_yes24, extract_book_data_aladin
+from github_utils import get_github_repo, upload_github_issue
+
+
+if __name__ == "__main__":
+    
+    ############## Setting
+    access_token = os.environ['MY_GITHUB_TOKEN']
+    repository_name = "github-action-with-python"
+    
+    ############## Timezone
+    seoul_timezone = timezone('Asia/Seoul')
+    today = datetime.now(seoul_timezone)
+    today_date = today.strftime("%Y년 %m월 %d일")
+    
+    ############## upload
+    ## ---- Yes24
+    issue_title_yes24 = f"YES24 IT 신간 도서 알림({today_date})"
+    upload_contents_yes24 = extract_book_data_yes24()
+    repo = get_github_repo(access_token, repository_name)
+    upload_github_issue(repo, issue_title_yes24, upload_contents_yes24)
+
+    ## ---- Aladin
+    issue_title_aladin = f"알라딘 IT 신간 도서 알림({today_date})"
+    upload_contents_aladin = extract_book_data_aladin()
+    repo = get_github_repo(access_token, repository_name)
+    upload_github_issue(repo, issue_title_aladin, upload_contents_aladin)
+
+    print("Upload Github Issue Success!")
+
+
